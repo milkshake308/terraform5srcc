f0775208d18689170de43b6d4b8861cff7bdc8e0
milkshake308 <milkshake.2734@gmail.com>
2024-11-04 12:56:01 +0100

    doc: explain what Name does

--- hello_world_vpc.tf
+++ hello_world_vpc.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "hello-world-5srcc" {
   cidr_block = "10.52.52.0/24"
   tags = {
-    Name = "HelloWorldSRCC"
+    Name = "HelloWorldSRCC" # This will print a nice name on the Web Console
   }
 }
