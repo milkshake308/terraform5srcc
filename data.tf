9d0adec4181293294544566a17d828c4a616752d
Aaron S <aaron@alfred-eyes.ai>
2024-11-05 17:08:11 +0100

    fix: if module not present data will fail

--- data.tf
+++ data.tf
@@ -1,11 +1,13 @@
 # Fetch the EKS cluster details
 data "aws_eks_cluster" "eks" {
   name = var.cluster_name
+  depends_on = [ module.eks-cluster ]
 }
 
 # Fetch the EKS authentication token
 data "aws_eks_cluster_auth" "eks" {
   name = var.cluster_name
+  depends_on = [ module.eks-cluster ]
 }
 
 data "external" "oidc_provider_fingerprint" {
